c2f0dfc305fee7ece2981ab124b4d362a50606a9
Thibf <thibaultferrante@hotmail.com>
2016-12-08 23:49:07 +0000

    Begin with librarian

---
+++ bin/librarian.py
@@ -0,0 +1,16 @@
+class Librarian():
+     """ Will know and manage your Music library """
+    def __init__(self,user):
+        self.user=user
+        self.musics=[]
+        self.fifo_upload=[]
+
+    def add_music(self,music):
+        self.queue_upload(music)
+        pass
+    def __upload__(self):
+        pass
+
+    def uploadSynchronously(self):
+        pass
+
