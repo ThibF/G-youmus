e2d4fc3965aeecdad07b2e5a25cd23f5d73fd605
ThibF <thibaultferrante@hotmail.com>
2017-07-11 23:24:59 +0200

    Revert "add logging"
    This reverts commit c0602c62b94ee2154df0d9faa57cc92def16b309.

--- consumerSQS/answer.py
+++ consumerSQS/answer.py
@@ -4,8 +4,6 @@ from string import Template
 import logging
 
 def send_message(txt,userId):
-    print(txt)
-    return
     curlCommand = Template ("curl -X POST -H \"Content-Type: application/json\" -d '{  \"recipient\": { \"id\": \"$userId\" },\"message\": { \"text\": \"$msg\"  }}' \"https://graph.facebook.com/v2.6/me/messages?access_token=$token\" ")
     request=curlCommand.substitute(userId=userId,msg=txt,token=config["tokenFacebook"])
     logging.debug(request)
